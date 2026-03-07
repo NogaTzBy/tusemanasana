02dd473cf62fe27e904ce1ed51f3d75e2d400c21
OneView <kougaragency@gmail.com>
2026-03-07 00:56:09 -0300

    Update Meta Pixel ID

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '892202163602936');
+              fbq('init', '1693636315094563');
               fbq('track', 'PageView');
             `,
           }}
